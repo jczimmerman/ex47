f08949f7de90f4d379949f94e06a8a3236d00246
Joey Zimmerman <joeyzimmerman17@gmail.com>
2020-02-11 14:34:09 -0500

    setup skeleton

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r") as f:
 
 
 setuptools.setup(
-    name="yourproject"
+    name="ex47"
     version="0.0.1",
-    author="your name",
-    author_email="your@email.com",
-    url="https://github.com/yourusername/yourproject",
-    description="what does yourproject do?",
+    author="Joseph Zimmerman",
+    author_email="joeyzimmerman17@gmail.com",
+    url="https://github.com/jczimmerman/ex47",
+    description="Practicing my unit tests",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
@@ -19,4 +19,3 @@ setuptools.setup(
     tests_require=['pytest'],
     python_requires='>=3.6',
 )
-
---
+++ tests/test_ex47.py
@@ -0,0 +1,4 @@
+import ex47
+
+def test_import():
+    assert ex47
